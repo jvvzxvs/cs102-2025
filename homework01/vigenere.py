fb493d95227f743289447db1a8867e89264b997b
jvvzxvs <nikita021282@gmail.com>
2025-10-16 14:50:27 +0300

    Реализован vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,19 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    key = keyword.lower()
+    if not keyword:
+        return plaintext
+    key_idx = 0
+
+
+    for char in plaintext:
+        if char.isalpha():
+            base = ord("A") if char.isupper() else ord("a")
+            shift = ord(key[key_idx % len(key)]) - ord('a')
+            ciphertext += chr((ord(char) - base + shift) % 26 + base)
+            key_idx += 1
+        else: ciphertext += char
     return ciphertext
 
 
@@ -24,5 +36,19 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    key = keyword.lower()
+    if not keyword:
+        return ciphertext
+    key_idx = 0
+
+    for char in ciphertext:
+        if char.isalpha():
+            base = ord("A") if char.isupper() else ord("a")
+            shift = ord(key[key_idx % len(key)]) - ord('a')
+            plaintext += chr((ord(char) - base - shift) % 26 + base)
+            key_idx += 1
+        else: plaintext += char
+    return plaintext
+
     return plaintext
